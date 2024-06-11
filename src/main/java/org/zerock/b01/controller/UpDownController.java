ccc0b8a257fc032657063cdec31aedd1ace12700
LeeHSeoK <gustjr7532@naver.com>
2024-06-11 11:24:09 +0900

    클래스 이름 변경 필요없는 클래스 삭제 게시글 수정시 이름 자동 변경 수정 로그인시 메인에 로그인 아이디 출력

---
+++ src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -0,0 +1,60 @@
+package org.zerock.b01.controller;
+
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.b01.dto.upload.UploadFileDTO;
+import org.zerock.b01.dto.upload.UploadResultDTO;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Log4j2
+public class UpDownController {
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO) {
+        if (uploadFileDTO.getFiles() != null) {
+
+            final List<UploadResultDTO> list = new ArrayList<>();
+
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                String originalName = multipartFile.getOriginalFilename();
+                log.info(originalName);
+                String uuid = UUID.randomUUID().toString();
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
+                boolean image = false;
+                try {
+                    multipartFile.transferTo(savePath);
+                    if (Files.probeContentType(savePath).startsWith("image")) {
+                        image = true;
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 100, 100);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                list.add(UploadResultDTO.builder()
+                                .uuid(uuid)
+                                .fileName(originalName)
+                                .img(image)
+                                .build());
+            });
+            return list;
+        }
+        return null;
+    }
+}
